e0f0e5dda7e8ee3abcf8f27cdbf5fc32e2ae3aa4
Kristaps Ritins <kritinsdev@gmail.com>
2022-09-11 00:01:22 +0300

    classes card styles

--- inc/queries.php
+++ inc/queries.php
@@ -19,13 +19,13 @@ function befit_classes_list()
                         <h3><?php the_title(); ?></h3>
                     </a>
 
-                    <p><?php the_field('class_info'); ?></p>
                     <?php
                     $start_time = get_field('start_time');
                     $end_time = get_field('end_time');
                     ?>
 
-                    <p><?php echo $start_time . " to " . $end_time; ?></p>
+                    <p><?php the_field('class_info'); ?> - <?php echo $start_time . " to " . $end_time; ?></p>
+
                 </div>
 
             </li>
--- page-classes.php
+++ page-classes.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <main class="container page section classes-index">
+    <h1>All Available Classes</h1>
     <?php befit_classes_list(); ?>
 </main>
 
